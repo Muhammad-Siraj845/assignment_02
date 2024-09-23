715b67754984bd64caba902b13d4b21b027ce4e7
ahmed ahmed <sirajalidnb@gmail.com>
2024-09-23 10:11:17 +0500

    first commit

---
+++ assignment_02/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import About from "@/components/About"
+export default function Home() {
+  return (
+  <div>
+  
+  <>
+  <Header />
+  <About />
+  </></div>
+  );
+}
